c4d078ea9c3f3b66ae489ae63838e8bf49c41e7e
Nezzar KEFIF <kefifnezzar@gmail.com>
2020-11-19 20:37:39 +0100

    exercices

---
+++ index.js
@@ -0,0 +1,37 @@
+// 1 - Ecrire une fonction qui prend un tableau en argument et retourne le tableau sans le premier élément
+
+function withoutFirst(array) {
+  
+}
+
+// 2 - Ecrire un fonction qui prend un tableau de chiffres en argument et retourne un tableau qui ne contien que les chiffres divisible par deux.
+
+function divisibleByTwo(params) {
+  
+}
+
+// 3 - Écrire une fonction qui prend un tableau avec deux nombres qui représentent la longeur et largeur d'un carré et détermine si il s'agit d'un carré ou d'un rectangle:
+// Ex:
+// ([16, 16]) => 'Carré'
+// ([10, 22]) => 'Rectangle
+
+function rectangleOrSquare(array) {
+
+}
+
+// 4 - Ecrire une fonction qui retourne le nom du fichier et son extension dans un objet à partir de l'URL
+// Ex: "C:/Projects/pil_tests/ascii/edabit.txt" => { name: "edabit", extension: 'txt' }
+
+function getFileName(url) {
+
+}
+
+// 5 - Ecrire qui emule le comportement d'une machine à sous.
+// Exectuer la fonction retourne un tableau de 4 elements au hasard et un string du résultat: "Perdu..." ou "JACKPOT !".
+// () => 
+function jackpot() {
+  const cards = ['$', '`£', '@', '€']
+
+}
+
+console.log(jackpot())
