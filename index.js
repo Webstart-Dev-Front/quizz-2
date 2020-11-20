8af77d9ae47dff3569fe9d1978a6f2fc64fd90ee
stephane-crepin00 <72788024+stephane-crepin00@users.noreply.github.com>
2020-11-20 10:22:55 +0100

    quizz2

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // 1 - Ecrire une fonction qui prend un tableau en argument et retourne le tableau sans le premier élément
 
 function withoutFirst(array) {
-  
+  return array.slice(1);
 }
 
 // 2 - Ecrire un fonction qui prend un tableau de chiffres en argument et retourne un tableau qui ne contien que les chiffres divisible par deux.
 
 function divisibleByTwo(params) {
-  
+  var arr= []
+
+  for (let i = 0; i < params.length; i++) {
+    if (params[i] % 2 === 0) {
+      arr.push(params[i])
+    }
+  }
+  return arr
 }
 
 // 3 - Écrire une fonction qui prend un tableau avec deux nombres qui représentent la longeur et largeur d'un carré et détermine si il s'agit d'un carré ou d'un rectangle:
@@ -16,14 +23,19 @@ function divisibleByTwo(params) {
 // ([10, 22]) => 'Rectangle
 
 function rectangleOrSquare(array) {
-
+  if (array[0] == array[1])
+    return "Carré"
+  return "Rectangle"
 }
 
 // 4 - Ecrire une fonction qui retourne le nom du fichier et son extension dans un objet à partir de l'URL
 // Ex: "C:/Projects/pil_tests/ascii/edabit.txt" => { name: "edabit", extension: 'txt' }
 
 function getFileName(url) {
+  var new_url = url.substring(url.lastIndexOf('/')+1)
+  new_url = new_url.split('.')
 
+  return arr = {clé: "name: ", valeur:new_url[0], clé: "extension: ", valeur:new_url[1]}
 }
 
 // 5 - Ecrire qui emule le comportement d'une machine à sous.
@@ -31,7 +43,41 @@ function getFileName(url) {
 // () => 
 function jackpot() {
   const cards = ['$', '`£', '@', '€']
+  var arr = []
+  for (let i = 0; i < 4; i++) {
+    arr.push(cards[Math.floor(Math.random() * cards.length)])
+  }
+  var isSame = true;
 
+  for (let i = 0; isSame && i < arr.length; i++) {
+    isSame = (arr[0] == arr[i])
+  }
+  if (isSame == false) {
+    return arr + " Perdu..."
+  } else {
+    return arr + " JACKPOT !"
+  }
 }
 
+function tryJackpot() {
+  let i = 0;
+  while(1) {
+    var returnString = jackpot()
+    var result = returnString.split(' ')
+    i++
+    if (result[1] == 'JACKPOT') return result[0] +  ` ${i} tentatives avant le JACKPOT !`
+  }
+}
+
+
+
+const cards = ['$', '`£', '@', '€']
+var arr = [1, 2, 3, 4, 5, 6, 7]
+
+console.log(withoutFirst(cards))
+console.log(divisibleByTwo(arr))
+console.log(rectangleOrSquare([12, 12]))
+console.log(rectangleOrSquare([12, 13]))
+console.log(getFileName("C:/Projects/pil_tests/ascii/edabit.txt"))
 console.log(jackpot())
+console.log(tryJackpot())
