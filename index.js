d1d5749784e42ced938a5cf77600716a2cc73af5
ryan-5199 <58774031+ryan-5199@users.noreply.github.com>
2020-11-20 10:27:26 +0100

    quizz

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 // 1 - Ecrire une fonction qui prend un tableau en argument et retourne le tableau sans le premier élément
 
 function withoutFirst(array) {
-  
+  let newTable = array.shift()
+  return array
 }
+
+console.log(withoutFirst([2, 3, 4]))
 
 // 2 - Ecrire un fonction qui prend un tableau de chiffres en argument et retourne un tableau qui ne contien que les chiffres divisible par deux.
 
 function divisibleByTwo(params) {
-  
+  return params.filter(params => params%2 ===  0)
 }
+
+console.log(divisibleByTwo([3, 6, 12]))
 
 // 3 - Écrire une fonction qui prend un tableau avec deux nombres qui représentent la longeur et largeur d'un carré et détermine si il s'agit d'un carré ou d'un rectangle:
 // Ex:
@@ -16,8 +21,14 @@ function divisibleByTwo(params) {
 // ([10, 22]) => 'Rectangle
 
 function rectangleOrSquare(array) {
-
+  if (array[0] == array[1]) {
+    return 'Carré'
+  } else {
+    return 'Rectangle'
+  }
 }
+
+console.log(rectangleOrSquare([10, 15]))
 
 // 4 - Ecrire une fonction qui retourne le nom du fichier et son extension dans un objet à partir de l'URL
 // Ex: "C:/Projects/pil_tests/ascii/edabit.txt" => { name: "edabit", extension: 'txt' }
@@ -31,6 +42,12 @@ function getFileName(url) {
 // () => 
 function jackpot() {
   const cards = ['$', '`£', '@', '€']
+  cards.sort()
+  if (cards[0] == cards[1] == cards[2] == cards[3]){
+    return cards + 'JACKPOT'
+  } else {
+    return cards + ' Perdu'
+  }
 
 }
 
